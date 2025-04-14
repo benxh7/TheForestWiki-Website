9ec0bd0fc5118fc935012759e5f5c0957bad63cc
ro.mina <ro.torresg@duocuc.cl>
2025-04-13 23:20:52 -0400

    cambio settings.py

--- TheForestWiki/settings.py
+++ TheForestWiki/settings.py
@@ -12,28 +12,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
-# Inicializa explícitamente Oracle Instant Client para conectarnos a Oracle Cloud
-# ATENCION: ESTO LO PUEDES BORRAR, SOLO LO USO YO POR PROBLEMAS EN MI PC
-import os
-import sys
-from telnetlib import LOGOUT
-
-import cx_Oracle
-from django.conf.global_settings import LOGIN_URL
-
-cx_Oracle.init_oracle_client(lib_dir=r"C:\Program Files\instantclient_23_7")
-
-if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TheForestWiki.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and available on your PYTHONPATH environment variable? "
-            "Did you forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -98,9 +76,9 @@ WSGI_APPLICATION = 'TheForestWiki.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.oracle',
-        'NAME': 'z879lkbuz2lrlw8s_high',
-        'USER': 'USER',
-        'PASSWORD': '59B?w3FmX#+T7PX',
+        'NAME': 'pmmr4hx3wyo8ewss_high',
+        'USER': 'ADMIN',
+        'PASSWORD': 'Oraclecloud123',
     }
 }
 
